f0108e4c630af9e76ff795ec91074730dc34aa52
hendro <hendro.tb@gmail.com>
2017-11-08 10:37:16 +0700

    add animated in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <router-view/>
+    <transition
+        mode="out-in"
+        enter-active-class="fadeIn"
+        leave-active-class="fadeOut"
+        appear>
+        <router-view class="animated"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -11,7 +17,12 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '~animate.css';
+.animated {
+  animation-duration: .377s;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
