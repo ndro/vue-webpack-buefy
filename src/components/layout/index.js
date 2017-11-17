7e4dc46ca741239245c05c7e40f2f095ff2e9f70
hendro <hendro.tb@gmail.com>
2017-11-17 11:02:19 +0700

    init gh-pages

--- src/components/layout/index.js
+++
@@ -1,9 +0,0 @@
-import NavBar from './NavBar'
-
-import MainContent from './MainContent'
-
-import InfoBar from './InfoBar'
-
-import FooterBar from './FooterBar'
-
-export { NavBar, MainContent, InfoBar, FooterBar }
